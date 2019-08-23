1857e8760ecc49bd3d0de9a6492fec743cbeccc7
samuraikun <tazitaziawawa@gmail.com>
2019-08-23 23:42:01 +0900

    add new note from ui

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
